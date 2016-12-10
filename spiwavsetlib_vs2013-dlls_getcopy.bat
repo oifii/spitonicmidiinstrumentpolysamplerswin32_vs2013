4d0ecbe19589cc1da5c3eace8023f6f7430e8c28
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-10 11:54:50 -0500

    first commit

---
+++ spiwavsetlib_vs2013-dlls_getcopy.bat
@@ -0,0 +1,5 @@
+copy ..\spiwavsetlib_vs2013\debug\spiwavsetlib_vs2013.dll .\spiwavsetlib_vs2013.dll
+copy ..\spiwavsetlib_vs2013\debug\spiwavsetlib_vs2013.dll debug\spiwavsetlib_vs2013.dll
+copy ..\spiwavsetlib_vs2013\release\spiwavsetlib_vs2013.dll release\spiwavsetlib_vs2013.dll
+copy ..\spiwavsetlib_vs2013\x64\debug\spiwavsetlib_vs2013.dll x64\debug\spiwavsetlib_vs2013.dll
+copy ..\spiwavsetlib_vs2013\x64\release\spiwavsetlib_vs2013.dll x64\release\spiwavsetlib_vs2013.dll
